d53bffb82048dd86ff3fca032e94f99aff7bbbfd
pgliniecki <przemyslaw.gliniecki@connectmedica.com>
2020-03-22 20:29:40 +0100

    WIP: Data Structures for defining transformations

--- src/transformations.rs
+++ src/transformations.rs
@@ -1,3 +1,5 @@
+use crate::transformations::bi::BiInputTransformation;
+use crate::transformations::mono::MonoInputTransformation;
 use crate::values::geolocation::GeoCoordinates;
 use crate::values::ValueHolder;
 
@@ -16,6 +18,52 @@ pub enum TransformationError {
     InvalidInputType(ValueHolder, InputOrder),
     CouldNotFindTimezone(GeoCoordinates),
     UnknownTimezone(String)
+
+}
+
+pub enum TransformationType {
+
+    SingleInput,
+    DoubleInput
+
+}
+
+pub struct TransformationDefinition {
+
+    id: i32,
+    transformation_type: TransformationType,
+    result_value_name: String
+
+}
+
+pub struct SingleInputTransformationDefinition {
+
+    transformation_definition_id: i32,
+    input_name: String,
+    transformation: MonoInputTransformation
+
+}
+
+pub struct DoubleInputTransformationDefinition {
+
+    transformation_definition_id: i32,
+    first_input_name: String,
+    second_input_name: String,
+    transformation: MonoInputTransformation
+
+}
+
+pub enum Transformation {
+
+    SingleInputTransformation(MonoInputTransformation),
+    DoubleInputTransformation(BiInputTransformation)
+
+}
+
+pub struct TransformationRequest {
+
+    definition: TransformationDefinition,
+    transformation: Transformation
 
 }
 
