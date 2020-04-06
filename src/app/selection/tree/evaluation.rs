5ccf4838668d8f6e1ab7ccf8efaf448471f7b8e7
pgliniecki <przemyslaw.gliniecki@connectmedica.com>
2020-04-06 11:11:44 +0200

    WIP: selection evaluator

--- src/app/selection/tree/evaluation.rs
+++ src/app/selection/tree/evaluation.rs
@@ -1,8 +1,8 @@
-use crate::app::selection::nodes::{SelectionNode, SelectionNodeAddress, SelectionNodeDelegate};
-use crate::app::selection::edges::{SelectionEdge, SelectionEdgeAddress, SelectionEdgeDelegate};
-use crate::app::values::ValuesPayload;
-use crate::app::selection::tree::SelectionTreeError;
 use crate::app::selection::addressable::Address;
+use crate::app::selection::edges::{SelectionEdge, SelectionEdgeAddress, SelectionEdgeDelegate};
+use crate::app::selection::nodes::{SelectionNode, SelectionNodeAddress, SelectionNodeDelegate, SelectionNodeError};
+use crate::app::selection::tree::SelectionTreeError;
+use crate::app::values::ValuesPayload;
 
 pub struct SelectionTreeEvaluator {
 
@@ -18,8 +18,20 @@ impl SelectionTreeEvaluator {
                            payload: &ValuesPayload) -> Result<Vec<i32>, SelectionTreeError> {
         let mut selection_nodes: Vec<&SelectionNode> = Vec::new();
         selection_nodes.push(&self.start_node);
-
-        return Result::Ok(selected_command_ids);
+        match self.handle(&mut selection_nodes, payload, start_node) {
+            Ok(_) => {
+                let mut selected_command_ids: Vec<i32> = Vec::new();
+                for node in selection_nodes {
+                    match node.select_content_command_id(payload) {
+                        Ok(command_id) => selected_command_ids.push(*command_id),
+                        Err(error) =>
+                            return Result::Err(SelectionTreeError::SelectionNodeError(error)),
+                    }
+                }
+                return Result::Ok(selected_command_ids);
+            },
+            Err(error) => return Result::Err(error),
+        }
     }
 
 
@@ -55,8 +67,27 @@ impl SelectionTreeEvaluator {
         };
     }
 
-    fn get_next(&self, node: &SelectionNode) -> &SelectionNode {
-
+    fn handle(&self,
+              to_be_evaluated: &mut Vec<&SelectionNode>,
+              payload: &ValuesPayload,
+              current: &SelectionNode) -> Result<(), SelectionTreeError> {
+        for address in current.get_outgoing_edges() {
+            match self.get_edge(address) {
+                Ok(edge) => {
+                    if edge.can_pass(payload) {
+                        match self.get_node(edge.get_next_selection_node()) {
+                            Ok(node) => {
+                                to_be_evaluated.push(node);
+                                return self.handle(to_be_evaluated, payload, node);
+                            },
+                            Err(error) => Result::Err(error),
+                        }
+                    }
+                },
+                Err(error) => return Result::Err(error),
+            }
+        }
+        Result::Ok(())
     }
 
 }
