140e4f4ca215a876aee3569d8a5b6a83a8046018
pgliniecki <przemyslaw.gliniecki@connectmedica.com>
2020-09-20 21:11:43 +0200

    Selector and sequence BTNodes

--- src/app/behavior/node/composite/selector.rs
+++ src/app/behavior/node/composite/selector.rs
@@ -2,11 +2,25 @@ use crate::app::behavior::node::{BehaviorTreeNode, BTNodeExecutionContext, BTNod
 use crate::app::behavior::tick::{TickStatus, TickError};
 use crate::app::behavior::node::composite::CompositeNode;
 
-pub struct SelectorCompositeNode;
+pub struct SelectorCompositeNode {
+
+    children: Vec<BTNode>
+
+}
 
 impl BehaviorTreeNode for SelectorCompositeNode {
     fn tick(&self, context: &BTNodeExecutionContext) -> Result<TickStatus, TickError> {
-        unimplemented!()
+        for child in self.get_children() {
+            match child.tick(context) {
+                Ok(status) => match status {
+                    TickStatus::Success => return Result::Ok(TickStatus::Success),
+                    TickStatus::Failure => {},
+                    TickStatus::Running => return Result::Ok(TickStatus::Running),
+                },
+                Err(err) => {},
+            }
+        }
+        return Result::Ok(TickStatus::Failure);
     }
 }
 
--- src/app/behavior/node/composite/sequence.rs
+++ src/app/behavior/node/composite/sequence.rs
@@ -7,12 +7,22 @@ pub struct SequenceCompositeNode;
 impl BehaviorTreeNode for SequenceCompositeNode {
 
     fn tick(&self, context: &BTNodeExecutionContext) -> Result<TickStatus, TickError> {
-        unimplemented!()
+        for child in self.get_children() {
+            match child.tick(context) {
+                Ok(status) => match status {
+                    TickStatus::Success => {},
+                    TickStatus::Failure => return Result::Ok(TickStatus::Failure),
+                    TickStatus::Running => return Result::Ok(TickStatus::Running),
+                },
+                Err(err) => return Result::Err(err),
+            }
+        }
+        Result::Ok(TickStatus::Success)
     }
 
 }
 
-impl CompositeNode for BehaviorTreeNode {
+impl CompositeNode for SequenceCompositeNode {
     fn get_children(&self, context: &BTNodeExecutionContext) -> &Vec<BTNode> {
         unimplemented!()
     }
