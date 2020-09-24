dad866407915a6dbb7c05497137a41a73e823a4e
pgliniecki <przemyslaw.gliniecki@connectmedica.com>
2020-09-24 22:16:19 +0200

    Condition decorator node

--- src/app/behavior/node/decorator/condition.rs
+++ src/app/behavior/node/decorator/condition.rs
@@ -1,7 +1,19 @@
-use crate::app::behavior::node::{BehaviorTreeNode, BTNodeExecutionContext};
+use crate::app::behavior::node::{BehaviorTreeNode, BTNodeExecutionContext, BTNode};
 use crate::app::behavior::tick::{TickStatus, TickError};
+use crate::app::behavior::node::decorator::DecoratorNode;
+use crate::app::conditions::ConditionExpressionWrapper;
 
-pub struct ConditionDecoratorNode;
+pub struct ConditionDecoratorNode {
+
+    condition: ConditionExpressionWrapper
+
+}
+
+impl DecoratorNode for ConditionDecoratorNode {
+    fn get_child(&self, context: &BTNodeExecutionContext) -> &BTNode {
+        unimplemented!()
+    }
+}
 
 impl BehaviorTreeNode for ConditionDecoratorNode {
     fn tick(&self, context: &BTNodeExecutionContext) -> Result<TickStatus, TickError> {
