89cdda2fd120318aaf211f9158f6019659769fda
pgliniecki <przemyslaw.gliniecki@connectmedica.com>
2020-10-06 21:57:22 +0200

    Simple execution service backed with Arbiters

--- src/app/execution/service.rs
+++ src/app/execution/service.rs
@@ -1,7 +1,8 @@
-use actix::{Arbiter};
-use rand::seq::SliceRandom;
 use std::future::Future;
+
+use actix::Arbiter;
 use futures_channel::oneshot::Canceled;
+use rand::seq::SliceRandom;
 use rand::thread_rng;
 
 pub struct ExecutionService {
@@ -29,15 +30,48 @@ impl ExecutionService {
         )
     }
 
-    pub fn exec<F, R>(&self, f: F) -> impl Future<Output = Result<R, Canceled>>
+    pub fn exec<F, R>(&self, f: F)
+                      -> Result<impl Future<Output = Result<R, Canceled>>, ExecutionServiceError>
         where
             F: FnOnce() -> R + Send + 'static,
             R: Send + 'static,
     {
         match self.arbiters.choose(&mut thread_rng()) {
-            None => panic!("Arbiters list should be never empty."),
-            Some(arbiter) => arbiter.exec(f)
+            None => Result::Err(ExecutionServiceError::GotEmptyArbitersCollection),
+            Some(arbiter) => Result::Ok(arbiter.exec(f))
         }
     }
 
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use std::task::Poll;
+
+    #[actix_rt::test]
+    async fn test_execution_service_builds_correctly() {
+        assert_eq!(ExecutionService::new(vec![]).is_err(), true);
+        assert_eq!(ExecutionService::new(vec![Arbiter::new()]).is_ok(), true);
+    }
+
+    #[actix_rt::test]
+    async fn test_execution_service_schedules_task() {
+        let expected = 43200;
+        match ExecutionService::new(vec![Arbiter::new()]) {
+            Ok(service) => {
+                let result =
+                    service.exec(move || expected as i32 + 10);
+                assert_eq!(result.is_ok(), true);
+                match result {
+                    Ok(future) => {
+                        assert_eq!(expected + 10, future.await.unwrap());
+                    },
+                    Err(_) => panic!("Got error spawning future!")
+                }
+            },
+            Err(_) => panic!("Got error building service!")
+        }
+    }
 }
