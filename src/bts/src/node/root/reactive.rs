c7390071e079a128ba27c98e26128f36e9612acd
Przemyslaw Gliniecki <pgliniecki@protonmail.ch>
2021-03-21 14:17:47 +0000

    Added logging for root tick id (#38)

--- src/bts/src/node/root/reactive.rs
+++ src/bts/src/node/root/reactive.rs
@@ -1,11 +1,12 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
+use uuid::Uuid;
 
 use crate::context::BTNodeExecutionContext;
 use crate::node::BehaviorTreeNode;
 use crate::node::decorator::reactive::ReactiveConditionDecoratorNode;
-use crate::tick::{TickError, TickStatus, TickHeader};
+use crate::tick::{TickError, TickHeader, TickStatus};
 
 pub struct ReactiveRootBTNode {
 
@@ -17,11 +18,15 @@ pub struct ReactiveRootBTNode {
 
 #[async_trait]
 impl BehaviorTreeNode for ReactiveRootBTNode {
+
     async fn do_tick(&self,
                      header: &TickHeader,
                      context: &BTNodeExecutionContext) -> Result<TickStatus, TickError> {
         loop {
-            let result = self.child.tick(header, context).await;
+            let new_header = header.with_new_root_tick_id(Uuid::new_v4());
+
+            let result = self.child.tick(&new_header, context).await;
+
             if let Ok(TickStatus::Success) = result {
                 continue;
             } else {
--- src/bts/src/node/root/until_stopped.rs
+++ src/bts/src/node/root/until_stopped.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
+use uuid::Uuid;
 
 use crate::context::BTNodeExecutionContext;
 use crate::node::{BehaviorTreeNode, BTNode};
@@ -26,11 +27,14 @@ impl UntilStoppedRootBTNode {
 
 #[async_trait]
 impl BehaviorTreeNode for UntilStoppedRootBTNode {
+
     async fn do_tick(&self,
                      header: &TickHeader,
                      context: &BTNodeExecutionContext) -> Result<TickStatus, TickError> {
         loop {
-            self.child.tick(header, context).await;
+            let new_header = header.with_new_root_tick_id(Uuid::new_v4());
+
+            self.child.tick(&new_header, context).await;
         }
     }
 
